27df0e8fd0336ebda177879c2cbc69b4c5fa0af2
Ángel Píñar Rivas <anglepiri22@gmail.com>
2021-05-11 13:57:52 +0200

    Añadido pytest

---
+++ v1/tests.py
@@ -0,0 +1,23 @@
+import unittest
+import json
+import sys, os.path
+
+from api import *
+
+class TestAPI1(unittest.TestCase): 
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_pred24(self):
+        response = self.app.get('/servicio/v1/prediccion/24horas/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_pred48(self):
+        response = self.app.get('/servicio/v1/prediccion/48horas/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_pred72(self):
+        response = self.app.get('/servicio/v1/prediccion/72horas/')
+        self.assertEqual(response.status_code, 200)
